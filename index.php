0c34fca41209780135b901fecec202a9bea9a41b
BatoBalint <balint.bato@gmail.com>
2021-10-15 15:01:57 +0200

    haladok

--- index.php
+++ index.php
@@ -5,10 +5,36 @@
 ?><!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <meta charset="utf-8">
     <title></title>
   </head>
-  <body>
+  <body class="bg-dark">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="input-group mb-3">
+            <span class="input-group-text w-25 text-center" id="basic-addon1">Marka</span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text w-25" id="basic-addon1">Modell</span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text w-25" id="basic-addon1">Ev</span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+          </div>
+        </div>
+        <div class="col-sm-6">
 
+        </div>
+
+      </div>
+    </div>
   </body>
 </html>
+
+
+/* marka, modell, henger, urtartalom, teljesitmeny, fogyasztas, sebesseg*/
